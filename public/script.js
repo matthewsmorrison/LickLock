1e0282c12aec8cca71201eadd60708daa68db5ae
Julian Vossen <julian.vossen@rwth-aachen.de>
2018-09-01 13:59:34 +0100

    Fix top mouth

--- public/script.js
+++ public/script.js
@@ -1,16 +1,15 @@
-$(function() {
-
- new Dragdealer('content-scroller', {
-    y: 1,
-    horizontal: false,
-    vertical: true,
-    animationCallback: function(x, y) {
-        if (y > 0.05) {
-            this.setValue(1,1)
-        } else {
-            this.disable()
-            window.location.href = "unlocked.html"
+window.onload = function () { 
+    new Dragdealer('content-scroller', {
+        y: 1,
+        horizontal: false,
+        vertical: true,
+        animationCallback: function(x, y) {
+            if (y > 0.05) {
+                this.setValue(1,1)
+            } else {
+                this.disable()
+                window.location.href = "unlocked.html"
+            }
         }
-    }
-  });
-})
+    });
+}
